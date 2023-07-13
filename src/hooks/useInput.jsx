a4c9982bd336b51385531440259f1bbd8501a4d8
Kurnia Kharisma Agung Samiadjie <kurniakharisma17@gmail.com>
2023-07-13 10:49:24 +0700

    CI test pass

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -11,5 +11,5 @@ export const useInput = (initialValue = '') => {
     setValue(target.innerHTML);
   };
 
-  return [value, onDivChangeHandler, onChangeHandler];
+  return [value, onChangeHandler, onDivChangeHandler];
 };
